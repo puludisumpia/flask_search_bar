62a3d6031c02faf15bea3355f93a896c553f89db
puludisumpia <puludisumpia@gmail.com>
2021-10-21 22:54:20 +0200

    Ajout

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
